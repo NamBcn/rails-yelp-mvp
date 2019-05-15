116860e59b3b2cc0b016880663deafb30ec78036
Nam Hai Chau <namhai.chau@gmail.com>
2019-05-15 15:22:36 +0200

    added restaurant view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,23 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new()
+  end
+
+  def create
+    @restaurant = restaurant.create(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
+    # private
+    #   def restaurant_params
+    #     params.require(:restaurant).permit(:title, :details, :completed)
+    #   end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '555 555 555',
+    reviews:        5
+
+  },
+
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '555 555 555',
+    reviews:        5
+
+  },
+
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '555 555 555',
+    reviews:        5
+
+  },
+
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '555 555 555',
+    reviews:        5
+
+  },
+
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '555 555 555',
+    reviews:        5
+
+  }]
