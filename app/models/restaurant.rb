ccb364762f535395c4a9790685b79230a44535fa
Nam Hai Chau <namhai.chau@gmail.com>
2019-05-15 12:39:41 +0200

    added validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,13 @@
 class Restaurant < ApplicationRecord
+   validates :name, uniqueness: true, presence: true
+   validates :address, presence: true
+   validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
 end
+
+# A restaurant must have at least a name, an address and a category.
+# The restaurant category should belong to a fixed list ["chinese", "italian", "japanese",
+#   "french", "belgian"].
+# A review must have a parent restaurant.
+# A review must have content and a rating. The rating should be a number between 0 and 5.
+# When a restaurant is destroyed, all of its reviews should be destroyed as well.
+# Validate all model tests before moving to the routing layer. You can use this command:
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,14 @@
 class Review < ApplicationRecord
+  validates :content, presence: true
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }
 end
+
+
+# validates :name, uniqueness: true, presence: true
+#  validates :address, presence: true
+#  validates :stars,
+
+
+#  t.text "content"
+#  t.integer "rating"
+#  t.integer "restaurant_id"
